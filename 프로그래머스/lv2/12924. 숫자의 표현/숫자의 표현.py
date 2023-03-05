50b76e16bb4ea198be2b5c34635f7df5b2cfd979
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-03-05 22:05:55 +0900

    [level 2] Title: 숫자의 표현, Time: 5.67 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(1,n+1):
+        sum = 0
+        for j in range(i,n+1):
+            sum += j
+            if sum == n:
+                sum = 0
+                answer +=1
+                break;
+            elif sum >n:
+                break
+            
+        
+    return answer
