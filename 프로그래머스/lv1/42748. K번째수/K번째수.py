3288903d195716f563db526d50638d4fa2715564
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-04-12 01:10:55 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    
+    for i in range(len(commands)):
+        arr = array[commands[i][0]-1:commands[i][1]]
+        arr.sort()
+        answer.append(arr[commands[i][2] - 1])
+    return answer
