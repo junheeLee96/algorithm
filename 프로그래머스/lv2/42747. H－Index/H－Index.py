62b1ec7dbb0023540040034f251d1da649a9e026
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-04-12 21:58:05 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,7 +1,7 @@
 def solution(citations):
     answer = 0
-    citations.sort(reverse=True)
+    citations.sort(reverse = True)
     for idx , c in enumerate(citations):
-        if idx >= c:
+        if idx>= c:
             return idx
     return len(citations)
