1e1ffd74f41b209c28f7cad5a9bcf1d3ac14ad2c
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-08-21 01:56:04 +0900

    [level 2] Title: 타겟 넘버, Time: 4.82 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,25 +1,33 @@
 def solution(numbers, target):
     answer = 0
+    max_idx = len(numbers) 
     
     
+    def dfs (condition, idx,number):
     
-    def dfs(idx, num,calc):
-        nonlocal answer
-        if idx > len(numbers) - 1:
-            if num == target:
+        if idx == max_idx:
+            if number == target :
+                nonlocal answer 
                 answer += 1
-            return
+                return
         
-        if calc == '-':
-            num = num - numbers[idx]
+            else:
+                return 
+        
+        
+        
+        if condition:
+            number += numbers[idx]
         else:
-            num = num + numbers[idx]
+            number -= numbers[idx]
         
-        dfs(idx+1, num, '-')
-        dfs(idx+1, num, '+')
-    
-    dfs(0, 0, '-')
-    dfs(0, 0, '+')
-    
-    
+        idx += 1
+        
+        dfs(False, idx, number)
+        dfs(True,idx,number)
+        
+        
+#         False는 빼기, True는 덧셈
+    dfs (False, 0, 0)
+    dfs (True, 0, 0)
     return answer//2
