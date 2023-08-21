2f9716b403e21d928550000643b73eed935c3417
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-08-21 22:52:43 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 64 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,45 +1,47 @@
 from collections import deque
 
-# 유저의 수와 관계의 개수를 입력 받음
-users, relationships = map(int, input().split())
+n, m = map(int, input().split())
 
-# 각 유저의 친구 관계를 저장하는 리스트
-arr = [[] for _ in range(users + 1)]
+arr = [[] for _ in range(n+1)]
 
-# 친구 관계를 입력 받아 리스트에 저장
-for _ in range(relationships):
+answer = [99998999999999999999999999999999999999999999, 0]
+
+# for i in range()
+
+for i in range(m):
     a, b = map(int, input().split())
+
     arr[a].append(b)
     arr[b].append(a)
 
-# 각 유저의 케빈 베이컨 수를 계산하는 함수
-def kevin_bacon(user):
+
+def bfs(idx):
     q = deque()
-    q.append((user, 0))
-    visited = [False] * (users + 1)
-    visited[user] = True
-    total_distance = 0
+
+    q.append((idx, 0))
+
+    sum_distance = 0
+    visit = [False for _ in range(n + 1)]
 
     while q:
-        curr_user, distance = q.popleft()
-        total_distance += distance
+        idx, distance = q.popleft()
+        visit[idx] = True
+        sum_distance += distance
 
-        for friend in arr[curr_user]:
-            if not visited[friend]:
-                q.append((friend, distance + 1))
-                visited[friend] = True
+        for i in arr[idx]:
+            if visit[i] == False:
+                # print(arr[idx])
 
-    return total_distance
+                q.append((i, distance + 1))
+                visit[i] = True
 
-min_kevin_bacon = float('inf')  # 최소 케빈 베이컨 수 초기화
-min_user = -1  # 최소 케빈 베이컨 수를 가진 유저 번호 초기화
+    return (sum_distance)
 
-# 각 유저의 케빈 베이컨 수를 계산하고, 가장 작은 값을 가진 유저 찾기
-for user in range(1, users + 1):
-    user_kevin_bacon = kevin_bacon(user)
-    
-    if user_kevin_bacon < min_kevin_bacon:
-        min_kevin_bacon = user_kevin_bacon
-        min_user = user
 
-print(min_user)
+for i in range(1, n + 1):
+    distance = bfs(i)
+    if answer[0] > distance:
+        answer = [distance, i]
+    # answer.append(distance)
+print(answer[1])
+# print(answer.index(min(answer)))
