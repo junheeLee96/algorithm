1e07cf3dc37fd784a53ad8e5f9cf08e45fdac3d4
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-08-27 19:51:35 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 152 ms, Memory: 36916 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,28 +1,26 @@
-def binary_search(ls: list, x: int) -> int:
-    start = 0
-    end = len(ls) - 1
-    while start + 1 < end:
-        mid = (start + end) // 2
-        if ls[mid] < x:
-            start = mid
-        elif ls[mid] >= x:
-            end = mid
-
-    if x <= ls[start]:
-        return 0
-    elif ls[start] < x <= ls[end]:
-        return end
-    else:
-        return len(ls)
-
+from bisect import bisect_left
 
 t = int(input())
-for tc in range(t):
-    a_length, b_length = map(int, input().split())
-    A = list(map(int, input().split()))
-    B = sorted(list(map(int, input().split())))
+
+answer = []
+
+for i in range(t):
+    n, m = map(int, input().split())
+
     cnt = 0
-    for a in A:
-        tmp = binary_search(B, a)
-        cnt += tmp
-    print(cnt)
+
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    a.sort()
+    b.sort()
+
+    for num in a:
+        idx = bisect_left(b, num)
+
+        cnt += idx
+
+    answer.append(cnt)
+
+for i in answer:
+    print(i)
