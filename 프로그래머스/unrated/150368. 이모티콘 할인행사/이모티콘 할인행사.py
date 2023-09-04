1c07a7e924c6749ef7019405b9a60120e6eb358b
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-09-04 16:12:30 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -2,30 +2,28 @@ from itertools import product
 
 
 def solution(users, emoticons):
-    answer = []
+    answer = [0,0]
     sales = [10, 20, 30, 40]    # 이모티콘의 할인율
 
-    answer = []
-
-    sales = [10, 20, 30, 40]
 
     for case in product(sales, repeat=len(emoticons)):
-        result = [0, 0]
-
+        plus = 0
+        money = 0
         for user in users:
-            temp = 0
-
+            total = 0
             for idx, sale in enumerate(case):
-                if sale >= user[0]:
-                    temp += emoticons[idx] * (100 - sale) // 100
-
-            if temp >= user[1]:
-                result[0] += 1
+                if case[idx] >= user[0]:
+                    total += emoticons[idx] * (100 - sale) // 100 
+                    
+            if user[1] <= total:
+                plus += 1
             else:
-                result[1] += temp
+                money += total
+        if answer[0] < plus:
+            answer[0] = plus
+            answer[1] = money
+        elif answer[0] == plus:
+            if answer[1] < money:
+                answer[1] = money
 
-        answer.append(result)
-
-    answer.sort(key=lambda x: (-x[0], -x[1]))
-
-    return answer[0]
+    return answer
