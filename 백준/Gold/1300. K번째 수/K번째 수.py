784894d455fbbbdeb69abc79edc83a45e7c4a393
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-11-14 01:04:53 +0900

    [Gold I] Title: K번째 수, Time: 1028 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -1,19 +1,21 @@
-N = int(input())
-K = int(input())
- 
-low = 0
-high = K#K번째 수는 K를 넘을수 없다.
-answer = 0
-while low <= high:
-    mid = (low + high)//2
-    count = 0
-    for i in range (1, N+1):
-        count = count + min(mid//i, N)#mid//i가N보다 클수 있기때문에 각줄이 N보다 넘게 포함할수는 없다.
- 
-    if count < K:
-        low = mid + 1
-    else:#최솟값을 찾아야하기때문에 같을때는 줄여준다.
+n = int(input())
+
+k = int(input())
+
+start = 0
+end = k
+
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 0
+    for i in range(1, n+1):
+        cnt += min(mid // i, n)
+
+    if cnt >= k:
         answer = mid
-        high = mid - 1
- 
+        end = mid - 1
+
+    else:
+        start = mid + 1
+
 print(answer)
