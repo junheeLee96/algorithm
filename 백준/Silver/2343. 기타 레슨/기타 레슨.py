f431eb3b663e92ca66485159a258a32b1f234999
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-11-14 22:10:00 +0900

    [Silver I] Title: 기타 레슨, Time: 600 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.py
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -1,40 +1,32 @@
-import sys
-input = sys.stdin.readline
-
 n, m = map(int, input().split())
-data = list(map(int, input().split()))
 
+arr = list(map(int, input().split()))
 
-# 순서를 유지해야 하므로 정렬을 사용하면 안된다.
-# 이분탐색을 두번 적용 시켜서
-num = sum(data)
-
-start = 0  # num // 3
-end = 10000000000
-result = num
-
+max_ray_len = max(arr)
+start = 1
+end = sum(arr)
+answer = sum(arr)
 while start <= end:
-    # mid 는 블루레이 크기
-    mid = (start+end) // 2
-    if mid < max(data):
+    mid = (start + end) // 2
+    if mid < max_ray_len:
         start = mid + 1
         continue
-    # cnt 는 블루레이 개수 , tmp 는 블루레이 갱신해주고 있는 블루레이 길이
-    cnt, tmp = 1, 0
-    # 하나씩 더하면서 갱신해준다.
-    for i in range(len(data)):
-        # 이전 값이랑 지금 값 더해서 mid 보다 작으면 계속 더해준다
-        if tmp + data[i] <= mid:
-            tmp += data[i]
-        # mid 보다 커지면 현재 data[i]가 tmp 로 들어가고
-        # 전에 있던 tmp는 0 초기화 해주고 개수 1개 늘려준다.
+
+    log = 0
+    cnt = 1
+
+    for i in range(len(arr)):
+        if log + arr[i] <= mid:
+            log += arr[i]
         else:
-            tmp = data[i]
             cnt += 1
-    # 개수가 m-1보다 작거나 같으면 이분탐색으로 영상 길이 인 mid값 갱신 로직 실행ㄴ
+            log = arr[i]
+        # print('mid = ', mid, 'arr[i] = ', arr[i], 'log = ', log)
+
     if cnt <= m:
         end = mid - 1
-        result = min(result, mid)
+        answer = min(answer, mid)
     else:
         start = mid + 1
-print(result)
+
+print(answer)
