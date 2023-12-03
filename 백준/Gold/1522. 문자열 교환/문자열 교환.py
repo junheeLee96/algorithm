50586a3cfa511c5f593701fe1f7767d2b68fe198
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-12-03 20:47:57 +0900

    [Gold V] Title: 문자열 교환, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1522. 문자열 교환/문자열 교환.py
+++ 백준/Gold/1522. 문자열 교환/문자열 교환.py
@@ -1,8 +1,14 @@
 s = input()  # 문자열 입력
 a = s.count('a')  # 입력된 str에서의 a의 개수
 
-s += s[0:a-1]  # 원형 문자열 처리
-min_val = float('inf')  # 최솟값
-for i in range(len(s)-(a-1)):
-    min_val = min(min_val, s[i:i+a].count('b'))
-print(min_val)
+answer = float('inf')
+
+
+s = s + s[0:a-1]
+
+
+for i in range(len(s) - (a-1)):
+
+    answer = min(answer, s[i:i+a].count('b'))
+
+print(answer)
