5ba62b1bb5bfa16db2326a6bae49407ab3a090f8
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-12-05 22:31:17 +0900

    [Silver II] Title: 상자넣기, Time: 40 ms, Memory: 33184 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left
+
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+dp = []
+
+
+for i in range(len(arr)):
+    if not dp or dp[-1] < arr[i]:
+        dp.append(arr[i])
+    else:
+        dp[bisect_left(dp, arr[i])] = arr[i]
+
+print(len(dp))
