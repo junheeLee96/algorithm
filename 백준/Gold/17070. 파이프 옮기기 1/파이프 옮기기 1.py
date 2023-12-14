6e5e49c77005793a846c140629c29c5ac9509ff0
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-12-14 23:01:24 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -0,0 +1,33 @@
+# 0 → ─, 1 → /, 2 → |
+
+def solution():
+
+    # 1행 미리 처리하기 → (3) 과정
+    dp[0][0][1] = 1
+    for i in range(2, N):
+        if board[0][i] == 0:
+            dp[0][0][i] = dp[0][0][i - 1]
+	
+    
+    # 왜 1행과 1열을 제외하는지는 (3), (4) 과정에서 봤었죠?
+    for r in range(1, N):
+        for c in range(1, N):
+            # (5) 과정
+            # 대각선 파이프를 추가하는 과정
+            if board[r][c] == 0 and board[r][c - 1] == 0 and board[r - 1][c] == 0:
+                dp[1][r][c] = dp[0][r - 1][c - 1] + dp[1][r - 1][c - 1] + dp[2][r - 1][c - 1]
+                
+	    # 가로, 세로 파이프를 추가하는 과정
+            if board[r][c] == 0:
+                dp[0][r][c] = dp[0][r][c - 1] + dp[1][r][c - 1]
+                dp[2][r][c] = dp[2][r - 1][c] + dp[1][r - 1][c]
+    
+    
+    # 최종 결과 출력
+    print(sum(dp[i][N - 1][N - 1] for i in range(3)))
+
+
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+dp = [[[0 for _ in range(N)] for _ in range(N)] for _ in range(3)]
+solution()
