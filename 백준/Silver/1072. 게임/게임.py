5a6700e92a5bbb3e27bc02573fb8317ed9f6ee06
이준희 <89452058+junheeLee96@users.noreply.github.com>
2023-12-21 22:16:02 +0900

    [Silver III] Title: 게임, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -1,25 +1,26 @@
-import math
+
 
 x, y = map(int, input().split())
 
-z = (y * 100) // x
+origin_ = (y * 100) // x
 
-if z >= 99:
+
+if origin_ >= 99:
     print(-1)
 else:
-    answer = math.inf  # 최솟값을 찾아야 하므로 무한대로 초기화
-
-    start, end = 1, 1000000000  # 이분 탐색의 범위를 수정
+    answer = float('inf')
+    start = 1
+    end = 1000000000
 
     while start <= end:
         mid = (start + end) // 2
+        l = ((y + mid) * 100) // (x + mid)
+        # print(mid, l)
 
-        new_z = ((y + mid) * 100) // (x + mid)
-
-        if new_z <= z:
-            start = mid + 1
-        else:
+        if l > origin_:
             end = mid - 1
             answer = min(answer, mid)
+        else:
+            start = mid + 1
 
     print(answer)
