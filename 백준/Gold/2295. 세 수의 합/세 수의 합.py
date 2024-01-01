011add00f1d0671a75c4353f356832d43abbf429
이준희 <89452058+junheeLee96@users.noreply.github.com>
2024-01-01 18:18:37 +0900

    [Gold IV] Title: 세 수의 합, Time: 300 ms, Memory: 65940 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+arr = [int(input()) for _ in range(N)]
+arr.sort()
+
+arr_sum = set()
+for x in arr:
+    for y in arr:
+        arr_sum.add(x+y)
+
+
+def check():
+    global answer
+    for i in range(N-1, -1, -1):
+        for j in range(i+1):
+            if arr[i]-arr[j] in arr_sum:
+                answer = arr[i]
+                return
+
+
+answer = 0
+check()
+print(answer)
