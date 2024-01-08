db81f179588c7d270b514c5d5de3ec1bdfa6a940
이준희 <89452058+junheeLee96@users.noreply.github.com>
2024-01-09 00:46:28 +0900

    [Silver I] Title: 어두운 건 무서워, Time: 1788 ms, Memory: 111192 KB -BaekjoonHub

---
+++ 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.py
@@ -0,0 +1,25 @@
+r, c, q = map(int, input().split())
+
+arr = []
+
+for _ in range(r):
+    arr.append(list(map(int, input().split())))
+
+
+dp = [[0 for _ in range(c+1)] for _ in range(r+1)]
+
+# print(dp)
+
+for i in range(1, r+1):
+    for j in range(1, c+1):
+        dp[i][j] = -dp[i-1][j-1] + dp[i][j-1] + dp[i-1][j] + arr[i-1][j-1]
+
+
+for _ in range(q):
+    r1, c1, r2, c2 = map(int, input().split())
+
+    num = dp[r2][c2] - dp[r1-1][c2] - dp[r2][c1-1] + dp[r1-1][c1-1]
+
+    div = ((r2 - r1) + 1) * ((c2 - c1) + 1)
+
+    print(num // div)
