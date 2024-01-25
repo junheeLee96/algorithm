ef322bd72f186e958c7aefa55d0b87217d0deb00
이준희 <89452058+junheeLee96@users.noreply.github.com>
2024-01-25 21:58:41 +0900

    [Gold V] Title: ABCDE, Time: 544 ms, Memory: 112084 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,54 +1,42 @@
-
-from sys import stdin
-input = stdin.readline
-
-# 입력값 받기
 n, m = map(int, input().split())
-relations = [[] for _ in range(n)]
-# 방문 표시
-visited = [False] * 2001
-# 정답 변수 생성
+
+arr = [[] for _ in range(n)]
+
 ans = False
-# 친구 관계 입력받기
-for i in range(m):
+
+visit = [False for _ in range(2001)]
+
+for _ in range(m):
     a, b = map(int, input().split())
-    # 친구 관계 등록
-    relations[a].append(b)
-    relations[b].append(a)
 
-# dfs 이용
-def dfs(idx, depth):
-    global ans
-    visited[idx] = True
-    # 친구 관계가 4번 이상 연결되어 있다면
-    if depth == 4:
-        # 정답 표시 후 리턴
+    arr[b].append(a)
+    arr[a].append(b)
+
+
+def dfs(idx, dep):
+    visit[idx] = True
+    if dep == 4:
+        global ans
         ans = True
-        return
-    # 친구 관계가 존재하는지 확인
-    for i in relations[idx]:
-        # 아직 방문하지 않았다면
-        if not visited[i]:
-            # 방문 표시
-            visited[i] = True
-            # 재귀적으로 수행
-            dfs(i, depth + 1)
-            # 방문 표시 해제
-            visited[i] = False
 
-# 0번부터 N-1번까지 확인하며
+        return
+
+    for i in arr[idx]:
+        if visit[i] == False:
+            visit[i] = True
+            dfs(i, dep + 1)
+
+            visit[i] = False
+
+
 for i in range(n):
-    # 친구 관계 탐색
     dfs(i, 0)
-    # 현재 방문 표시 해제
-    visited[i] = False
-    # 친구 관계가 존재한다면
+    visit[i] = False
     if ans:
-        # 탈출
         break
-# 정답이 있다면 1 출력
+
+
 if ans:
     print(1)
-# 정답이 없다면 0 출력
 else:
     print(0)
