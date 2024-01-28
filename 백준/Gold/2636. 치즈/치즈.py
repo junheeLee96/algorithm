70401bfaf970a3db03448a3b91e255175384741a
이준희 <89452058+junheeLee96@users.noreply.github.com>
2024-01-28 22:13:28 +0900

    [Gold IV] Title: 치즈, Time: 88 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -1,29 +1,29 @@
 from collections import deque
+import sys
 
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-
 arr = []
 
-for i in range(n):
-    a = list(map(int, input().split()))
-    arr.append(a)
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
 
+dx = [0, 0, 1, -1]
+dy = [-1, 1, 0, 0]
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-time = 0
-ans = []
+cnt = 0
 
 
 def bfs():
-    q = deque()
-    cnt = 0
-    q.append((0, 0))
+    visit = [[False for _ in range(m)] for _ in range(n)]
     visit[0][0] = True
+    q = deque()
+
+    q.append((0, 0))
+
+    lists = []
 
     while q:
         x, y = q.popleft()
@@ -31,28 +31,33 @@ def bfs():
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and visit[nx][ny] == 0:
+
+            if 0 <= nx < n and 0 <= ny < m and visit[nx][ny] == False:
                 if arr[nx][ny] == 0:
                     visit[nx][ny] = True
                     q.append((nx, ny))
-
                 elif arr[nx][ny] == 1:
-                    arr[nx][ny] = 0
                     visit[nx][ny] = True
-                    cnt += 1
+                    lists.append([nx, ny])
 
-    ans.append(cnt)
-    return cnt
+    return lists
+# while True:
 
+
+last = 0
 
 while True:
-    time += 1
+    cnt += 1
+    lists = bfs()
 
-    visit = [[False for _ in range(m)]for _ in range(n)]
-    cnt = bfs()
-
-    if cnt == 0:
+    if lists == []:
         break
 
-print(time - 1)
-print(ans[-2])
+    last = len(lists)
+
+    for i in lists:
+        x, y = i[0], i[1]
+
+        arr[x][y] = 0
+
+print(cnt-1, last)
