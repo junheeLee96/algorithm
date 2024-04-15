eedd973cb9d42c2b72ee6eb6f0abdeb5f77585ba
이준희 <89452058+junheeLee96@users.noreply.github.com>
2024-04-16 00:40:29 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = ''
+    
+    # s = s.split()
+    
+#     for i in range(len(s)):
+#         s[i] = s[i].capitalize()
+    
+#     answer= ' '.join(s)
+        
+    answer = ''
+    s = s.split(' ')
+    for i in range(len(s)):
+        s[i] = s[i].capitalize()
+    answer=' '.join(s)
+    return answer
