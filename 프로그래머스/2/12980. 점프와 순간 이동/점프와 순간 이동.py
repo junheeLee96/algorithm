5c443e04144a9e9b3f874c9420e22ab7405073ce
이준희 <89452058+junheeLee96@users.noreply.github.com>
2024-04-18 23:15:51 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,42 @@
+def solution(n):
+    ans = 0
+    
+    
+    
+    
+    while n>0:
+        ans += n % 2
+        n = n //2
+    return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(n):
+#     answer = 0
+#     while n >0:
+#         answer += n % 2
+#         n = n // 2
+        
+        
+    
+    
+
+#     return answer
