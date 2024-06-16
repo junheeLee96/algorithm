e6328df2d7d33e2c5a4e0bbc9daabaf5a1b3ba36
이준희 <89452058+junheeLee96@users.noreply.github.com>
2024-06-16 18:56:10 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,26 +1,19 @@
-from collections import deque
-
 a, b = map(int, input().split())
 
 r = 1
 
+while (b != a):
+    r += 1
+    temp = b
+    if b % 10 == 1:
+        b //= 10
+    elif b % 2 == 0:
+        b //= 2
 
-def bfs(a, b):
-    q = deque()
-    q.append((a, 1))
-
-    while q:
-        a, count = q.popleft()
-
-        if a == b:
-            print(count)
-            return
-
-        if a * 2 <= b:
-            q.append((a * 2, count + 1))
-        if a * 10 + 1 <= b:
-            q.append((a * 10 + 1, count + 1))
-    print(-1)
+    if temp == b:
+        print(-1)
+        break
 
 
-bfs(a, b)
+else:
+    print(r)
