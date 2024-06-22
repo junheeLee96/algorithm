55573ccc8078d591dba11adbd4c2353a23308412
이준희 <89452058+junheeLee96@users.noreply.github.com>
2024-06-23 00:10:37 +0900

    [Gold V] Title: 평범한 배낭, Time: 5648 ms, Memory: 229084 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,7 +1,3 @@
-import sys
-
-input = sys.stdin.readline
-
 n, k = map(int, input().split())
 
 arr = [[0, 0]]
@@ -9,16 +5,17 @@ arr = [[0, 0]]
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+dp = [[0] * (k + 1) for _ in range(n+1)]
+
 
 for i in range(1, n + 1):
-    for j in range(1, k+1):
+    for j in range(1, k + 1):
         w = arr[i][0]
         v = arr[i][1]
 
         if w > j:
             dp[i][j] = dp[i-1][j]
         else:
-            dp[i][j] = max(dp[i-1][j], v + dp[i-1][j-w])
+            dp[i][j] = max(dp[i-1][j], v + dp[i-1][j - w])
 
 print(dp[n][k])
