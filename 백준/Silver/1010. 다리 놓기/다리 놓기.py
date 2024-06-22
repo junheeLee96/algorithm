f7eec0554bff755994152de3cd9cea4f80305303
이준희 <89452058+junheeLee96@users.noreply.github.com>
2024-06-22 23:22:57 +0900

    [Silver V] Title: 다리 놓기, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+dp = [[0] * 30 for _ in range(30)]
+for i in range(30):
+    for j in range(30):
+        if i == 1:
+            dp[i][j] = j
+        else:
+            if i == j:
+                dp[i][j] = 1
+            elif i < j:
+                dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
+
+for _ in range(t):
+    n, m = map(int, input().split())
+    print(dp[n][m])
