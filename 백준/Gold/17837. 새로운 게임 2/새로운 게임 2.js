e04011d0511caaaeff2726adffc0b5b281b57748
이준희 <89452058+junheeLee96@users.noreply.github.com>
2025-02-27 23:15:09 +0900

    [Gold II] Title: 새로운 게임 2, Time: 192 ms, Memory: 12572 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.js
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.js
@@ -6,75 +6,79 @@ const rl = readline.createInterface({
 });
 
 let n, k;
-let board = [];
-let maps = [];
-let pieces = [];
+let arr = [];
+let temp = 0;
+let temp2 = 0;
 
-const dx = [0, 0, -1, 1]; // 우, 좌, 상, 하
+const dx = [0, 0, -1, 1]; // 오 왼 위 아래
 const dy = [1, -1, 0, 0];
+let maps = [];
+let info = [];
 
 rl.on("line", (line) => {
     if (!n) {
         [n, k] = line.split(" ").map(Number);
-        board = [];
         maps = Array.from({ length: n }, () => Array.from({ length: n }, () => []));
-    } else if (board.length < n) {
-        board.push(line.split(" ").map(Number));
+    } else if (temp < n) {
+        arr.push(line.split(" ").map(Number));
+        temp++;
     } else {
         let [x, y, dir] = line.split(" ").map(Number);
-        pieces.push([x - 1, y - 1, dir - 1]);
-        maps[x - 1][y - 1].push(pieces.length - 1);
+        info.push([x - 1, y - 1, dir - 1]);
+        maps[x - 1][y - 1].push(temp2);
+        temp2++;
     }
 }).on("close", () => {
     console.log(solution());
 });
 
 function solution() {
-    let turn = 0;
+    let cnt = 0;
 
-    while (turn <= 1000) {
-        turn++;
+    while (cnt <= 1000) {
+        cnt++;
 
         for (let i = 0; i < k; i++) {
-            let [x, y, dir] = pieces[i];
+            let [x, y, dir] = info[i];
 
             let nx = x + dx[dir];
             let ny = y + dy[dir];
 
-            // 범위를 벗어나거나 파란색일 경우 방향 반전
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n || board[nx][ny] === 2) {
+            // 범위를 벗어나거나 파란색(2)을 만나면 방향 반전
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n || arr[nx][ny] === 2) {
                 dir = dir % 2 === 0 ? dir + 1 : dir - 1;
                 nx = x + dx[dir];
                 ny = y + dy[dir];
 
-                pieces[i][2] = dir;
+                info[i][2] = dir; // 방향 업데이트
 
-                // 반대 방향 이동도 파란색이거나 범위를 벗어나면 제자리 유지
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n || board[nx][ny] === 2) {
+                // 반대 방향도 이동 불가하면 제자리 유지
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n || arr[nx][ny] === 2) {
                     continue;
                 }
             }
 
-            // 현재 위치에서 말들 이동
-            let idx = maps[x][y].indexOf(i);
-            let movingPieces = maps[x][y].splice(idx);
+            // 현재 위치에서 이동할 말들 찾기
+            const idx = maps[x][y].indexOf(i);
+            const children = maps[x][y].splice(idx);
 
-            // 말들의 위치 업데이트
-            for (let piece of movingPieces) {
-                pieces[piece][0] = nx;
-                pieces[piece][1] = ny;
+            // 이동하는 말들의 좌표 갱신
+            for (let child of children) {
+                info[child][0] = nx;
+                info[child][1] = ny;
             }
 
-            // 빨간색이면 순서 뒤집음
-            if (board[nx][ny] === 1) {
-                movingPieces.reverse();
+            // 빨간색(1)이면 순서 뒤집기
+            if (arr[nx][ny] === 1) {
+                children.reverse();
             }
 
-            maps[nx][ny] = [...maps[nx][ny], ...movingPieces];
+            // 새로운 위치에 말 추가
+            maps[nx][ny].push(...children);
 
-            // 한 칸에 말이 4개 이상이면 종료
+            // 게임 종료 조건: 한 칸에 말이 4개 이상 쌓이면 종료
             if (maps[nx][ny].length >= 4) {
-                return turn;
+                return cnt;
             }
         }
     }
